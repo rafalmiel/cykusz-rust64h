90f3d845b74602418af8faf7dd801490de8fb83a
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2016-06-06 21:55:23 +0100

    Add rust-x86 crate as a git submodule

--- update_core_nightly.sh
+++ update_core_nightly.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-rustup update nightly-2016-05-22
-rustup override add nightly-2016-05-22
-#rustup update nightly
-#rustup override add nightly
+#rustup update nightly-2016-05-22
+#rustup override add nightly-2016-05-22
+rustup update nightly
+rustup override add nightly
+git submodule update
 pushd .
 cd ../rust
 git pull
 git submodule update
-git checkout 476fe6eef
-#git checkout master
+#git checkout 476fe6eef
+git checkout master
 popd
 mkdir -p ./build
 rustc --target x86_64-unknown-none-gnu -Z no-landing-pads --out-dir ./build ../rust/src/libcore/lib.rs
