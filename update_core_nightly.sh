514b0b7202508596e10a89c1660bf8f38406ed4b
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2016-06-26 22:47:28 +0100

    travis: Fix travis filename

--- update_core_nightly.sh
+++ update_core_nightly.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+rust_dir=$1
+
 #rustup update nightly-2016-05-22
 #rustup override add nightly-2016-05-22
 rustup update nightly
 rustup override add nightly
 git submodule update
 pushd .
-cd ../rust
+cd $rust_dir
 git checkout master
 git submodule update
 git pull
 #git checkout 476fe6eef
 popd
 mkdir -p ./build
-rustc --target x86_64-unknown-none-gnu -Z no-landing-pads --out-dir ./build ../rust/src/libcore/lib.rs
+rustc --target x86_64-unknown-none-gnu -Z no-landing-pads --out-dir ./build ${rust_dir}/src/libcore/lib.rs
