260e0b6b75e672f66a335ced6110a2efe2a0bc66
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2017-04-25 00:47:10 +0100

    sync: Fix code style

--- src/arch/x86_64/sync.rs
+++ src/arch/x86_64/sync.rs
@@ -12,17 +12,15 @@ pub struct MutexGuard<'a, T: ?Sized + 'a> {
 
 impl<T> Mutex<T> {
 
-pub const fn new(user_data: T) -> Mutex<T>
-{
-    Mutex
-    {
-        l: M::new(user_data),
+    pub const fn new(user_data: T) -> Mutex<T> {
+        Mutex {
+            l: M::new(user_data),
+        }
     }
-}
+
     pub fn lock(&self) -> MutexGuard<T> {
         task_locked();
-        MutexGuard
-        {
+        MutexGuard {
             g: Some(self.l.lock()),
         }
     }
@@ -30,11 +28,15 @@ pub const fn new(user_data: T) -> Mutex<T>
 
 impl<'a, T: ?Sized> Deref for MutexGuard<'a, T> {
     type Target = T;
-    fn deref<'b>(&'b self) -> &'b T { self.g.as_ref().unwrap() }
+    fn deref<'b>(&'b self) -> &'b T {
+        self.g.as_ref().unwrap()
+    }
 }
 
 impl<'a, T: ?Sized> DerefMut for MutexGuard<'a, T> {
-    fn deref_mut<'b>(&'b mut self) -> &'b mut T { self.g.as_mut().unwrap() }
+    fn deref_mut<'b>(&'b mut self) -> &'b mut T {
+        self.g.as_mut().unwrap()
+    }
 }
 
 impl<'a, T: ?Sized> Drop for MutexGuard<'a, T> {
